cec6ca8fcb6a9fc9481cca9dc8ce0aa22cb1e9d0
shu-miao <3120929033@qq.com>
2025-04-08 14:40:16 +0800

    新增王正非模型

--- Cellular_automata/CellularAutomaton.py
+++ Cellular_automata/CellularAutomaton.py
@@ -1,7 +1,40 @@
 import numpy as np
 import pandas as pd
+from util.WangZhengfei_Model import WangZhengfei_Model
+
+class Fire_cellular:
+    def __init__(self,T_max,W_mean,h_min,s,w,p,lat,lon):
+        '''
+        :param T_max: 日最高气温
+        :param W_mean: 中午平均风级
+        :param h_min: 日最小相对湿度
+        :param s: 可燃物类型
+        :param w: 风速
+        :param p: 坡度（角度）
+        :param lat: 经度
+        :param lon: 维度
+        '''
+        self.T_max = T_max
+        self.W_mean = W_mean
+        self.h_min = h_min
+        self.s = s
+        self.w = w
+        self.p = p
+        self.lat = lat
+        self.lon = lon
 
 class CellularAutomaton:
-    def __init__(self,grid_size,lat,lon):
+    def __init__(self,grid_size,granularity):
+        '''
+        :param grid_size: 沙盘大小
+        :param granularity: 经纬度粒度
+        '''
         self.grid_size = grid_size
+        self.granularity = granularity
         self.grid = np.zeros((grid_size, grid_size))
+    def update(self,fire_cellular:Fire_cellular):
+        '''
+        :param fire_cellular: 火蔓延参数
+        :return:
+        '''
+
--- util/WangZhengfei_Model.py
+++ util/WangZhengfei_Model.py
@@ -1,7 +1,47 @@
 import numpy
 import math
 
-class WangZhengfei_Model:
+def WangZhengfei_Model(T_max,W_mean,h_min,s,w,p):
+    '''
+    :param T_max: 日最高气温
+    :param W_mean: 中午平均风级
+    :param h_min: 日最小相对湿度
+    :param s: 可燃物类型
+    :param w: 风速
+    :param p: 坡度（角度）
+    :return: 研究区域可燃物下林火蔓延速率
+    '''
+    Ks_dict = {
+        '平铺松针': 0.8,
+        '枯枝落叶': 1.2,
+        '茅草杂草': 1.6,
+        '莎草矮桦': 1.8,
+        '牧场草原': 2.0,
+        '红松、华山松、云南松等林地': 1.0
+    }
+    Ks = Ks_dict.get(s, 1.0)  # 可燃物综合物理性质与化学性质的燃烧性系数 默认值为1.0
+    Kw_dict = {
+        (1, 1): 1.2,
+        (1, 2): 1.4,
+        (2, 3): 1.7,
+        (3, 4): 2.0,
+        (4, 5): 2.4,
+        (5, 6): 2.9,
+        (6, 7): 3.3,
+        (7, 8): 4.1,
+        (8, 9): 5.1,
+        (9, 10): 6.0,
+        (10, 11): 7.3,
+        (11, 12): 8.5
+    }
+    Kw = next((value for (lower, upper), value in Kw_dict.items() if lower < w <= upper), 8.5) # # 风速调整系数
+    R0 = 0.03 * T_max + 0.05 * W_mean + 0.01 * h_min - 0.3 # 日初始蔓延速率
+    p_rad = math.radians(p)  # 转为弧度
+    Kp = math.e ** (3.533 * (math.tan(p_rad) ** 1.2))  # 坡度调整系数
+    R = R0 * Ks * Kw * Kp # 计算蔓延速率
+    return R
+
+class WangZhengfeiModel:
     def __init__(self,T,W,h):
         '''
         :param T: 日最高气温
@@ -24,7 +64,6 @@ class WangZhengfei_Model:
         :param p: 坡度（角度）
         :return: R 研究区域可燃物下林火蔓延速率
         '''
-
 
         Ks_dict = {
             '平铺松针': 0.8,
@@ -59,4 +98,3 @@ class WangZhengfei_Model:
         self.R = self.R0 * self.Ks * self.Kw * self.Kp # 计算蔓延速率
 
         return self.R
-
